2c8cf5fd9c53e91277f3e8706a5ca473797c74d4
dehapekin <dehapekin@gmail.com>
2025-10-31 10:17:11 +0400

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-31T06:17:13.354Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-10-31T06:17:14.877Z
+Commit: Patch security vulnerability in authentication
